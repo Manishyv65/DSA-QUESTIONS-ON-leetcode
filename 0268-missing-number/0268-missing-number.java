202f7366c760bc21e12f39c3071a74015f73b314
Manish Kumar <manishyadav00772@gmail.com>
2026-08-15 21:52:49 +0530

    Time: 0 ms (100%), Space: 47.5 MB (33.47%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,20 +1,33 @@
+// class Solution {
+//     public int missingNumber(int[] nums) {
+//         int n = nums.length;
+//         for(int i=0; i<=n; i++){
+//             boolean found = false;
+//             for(int j =0; j<n; j++){
+//                 if(nums[j] == i){
+//                     found = true;
+//                     break;
+//                 }
+//             }
+//             if (!found){
+//                 return i;
+//             }
+
+//         }
+//         return -1;
+
+//     }
+// }
 class Solution {
     public int missingNumber(int[] nums) {
+
         int n = nums.length;
-        for(int i=0; i<=n; i++){
-            boolean found = false;
-            for(int j =0; j<n; j++){
-                if(nums[j] == i){
-                    found = true;
-                    break;
-                }
-            }
-            if (!found){
-                return i;
-            }
+        int missing = n;
 
+        for (int i = 0; i < n; i++) {
+            missing = missing ^ i ^ nums[i];
         }
-        return -1;
 
+        return missing;
     }
 }
